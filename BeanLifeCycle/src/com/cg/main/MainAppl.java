828263b72daff83b0fc8c81d0681146475c84d8c
Nikkithakur <Nikkithakur@users.noreply.github.com>
2018-08-21 10:48:45 +0530

    Add files via upload

---
+++ BeanLifeCycle/src/com/cg/main/MainAppl.java
@@ -0,0 +1,25 @@
+package com.cg.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cg.beans.Employee;
+
+public class MainAppl
+{
+ 
+	public static void main(String[] args)
+	{
+	
+		AbstractApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		context.registerShutdownHook();
+		Employee emp=(Employee)context.getBean("employee");
+		System.out.println(emp.getEmplNumber());
+		System.out.println(emp.getName());
+	}
+	
+	
+	
+	
+}
---
+++ BeanLifeCyclePOJO/src/com/cg/beans/Employee.java
@@ -0,0 +1,37 @@
+package com.cg.beans;
+
+
+
+public class Employee  
+{
+private int emplNumber;
+String name;
+public int getEmplNumber() {
+	return emplNumber;
+}
+public void setEmplNumber(int emplNumber) {
+	this.emplNumber = emplNumber;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Employee() {
+	System.out.println("Employee object created");
+}
+
+public void destroy()  {
+	System.out.println("Destoy");
+	
+}
+
+public void init()  {
+	System.out.println("Init");
+	
+}
+
+
+	
+}
