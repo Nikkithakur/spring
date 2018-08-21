01c483f70075fe64380e26562ed7457823e1f0b9
Nikkithakur <Nikkithakur@users.noreply.github.com>
2018-08-21 10:45:01 +0530

    Add files via upload

---
+++ Inheritance/src/com/cg/beans/Employee.java
@@ -0,0 +1,58 @@
+package com.cg.beans;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Employee {
+	private int emplNumber;
+	private String name;
+	@Autowired
+	Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Employee(int emplNumber) {
+		super();
+		this.emplNumber = emplNumber;
+	}
+
+	public Employee(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Employee(int emplNumber, String name) {
+		super();
+		this.emplNumber = emplNumber;
+		this.name = name;
+	}
+
+	public int getEmplNumber() {
+		return emplNumber;
+	}
+
+	public void setEmplNumber(int emplNumber) {
+		this.emplNumber = emplNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Employee() {
+		System.out.println("Employee object created");
+	}
+
+}
---
+++ Inheritance/src/com/cg/main/MainAppl.java
@@ -0,0 +1,25 @@
+package com.cg.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cg.beans.Employee;
+
+public class MainAppl
+{
+ 
+	public static void main(String[] args)
+	{
+	
+		ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee emp=(Employee)context.getBean("employee");
+		System.out.println(emp.getEmplNumber());
+		System.out.println(emp.getName());
+		System.out.println(emp.getAddress().getCity());
+		
+	}
+	
+	
+	
+	
+}
